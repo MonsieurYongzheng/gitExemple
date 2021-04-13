e3e22ec9c191f7beaeb8c6822074371b4cc7ba6f
yangjoanne216 <yangjoanne216@gmail.com>
2021-04-13 23:40:48 +0200

    v2

--- issus1.py
+++ issus1.py
@@ -1,3 +1,8 @@
+import os
 from git import Repo
 repo = Repo.clone_from(url='git@github.com:Legilibre/Archeo-Lex.git',to_path='../Newnew')
 remote = repo.create_remote(name='gitlab', url='git@github.com:Legilibre/Archeo-Lex.git')
+with open(os.path.join(rw_dir,'Newnew.tar'),'wb') as fp:
+    repo.archive(fp)
+
+if else
